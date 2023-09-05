1480305d959daa420542d72b56d252287152a3d6
Crisslope01 <122842614+Crisslope01@users.noreply.github.com>
2023-09-05 01:34:07 -0300

    actualizacion

---
+++ scraping/stackoverflow.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+encabezado = {
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36",
+}
+
+url = "https://stackoverflow.com/questions"
+
+respuesta = requests.get(url, headers = encabezado)
+
+soup = BeautifulSoup(respuesta.text)
+
+contenedor_de_preguntas = soup.find("div", id="questions")
+
+lista_de_preguntas = contenedor_de_preguntas.find_all("div", class_="s-post-summary")
+
+for pregunta in lista_de_preguntas:
+   texto_pregunta = pregunta.find("h3").text
+   descripcion_pregunta = pregunta.find("div", class_ = "s-post-summary--content-excerpt").text
+   descripcion_pregunta = descripcion_pregunta.replace("\n", "").replace("\r", "").strip()
+   print(texto_pregunta)
+   print(descripcion_pregunta)
+   print()
--- scraping/wikipedia.py
+++ scraping/wikipedia.py
@@ -12,18 +12,39 @@ from lxml import html
 #es una cadena de tecto con la cual se puede identificar el navegador y el sistema operativo del cliente. por defecto es:ROBOT 
 # e identifica la maquina de inmediato que se esta haciendo escrapeo por lo que hay que modificarla antes de hacer la consulta o request
 
+
+ #- Aprender a utilizar requests para hacer requerimientos
 encabezados = {
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36",
 }
-
 # Empezamos definiedo la URL semilla de la página que queremos escrapear
-
 url= "https://www.wikipedia.org/"
 
-#aca estamos capturando el requerimiento de la pagina
-
+#aca estamos capturando la consulta a la pagina
 respuesta = requests.get(url, headers=encabezados)
 
-print(respuesta) 
+#print(respuesta.text) 
 
+ #- Aprender a utilizar lxml para parsear el arbol HTML
+ 
+parser = html.fromstring(respuesta.text)
 
+ingles =parser.get_element_by_id("js-link-box-en")
+ 
+#print(ingles.text_content())
+
+ex_ingles = parser.xpath("//a[@id='js-link-box-en']/strong/text()") #xpath es una forma de buscar en el arbol html
+
+#print(ex_ingles)
+
+extraer_idiomas = parser.xpath("//div[contains(@class, 'central-featured-lang')]//strong/text()") #contains es una funcion de xpath que permite buscar por una cadena de texto
+
+#print(extraer_idiomas)
+
+for idioma in extraer_idiomas:
+    print(idioma)
+
+extraer_idiomas_otra_forma = parser.find_class("central-featured-lang") #find_class es una funcion de lxml que permite buscar por una clase
+
+for idiomas in extraer_idiomas_otra_forma:
+    print(idiomas.text_content())
