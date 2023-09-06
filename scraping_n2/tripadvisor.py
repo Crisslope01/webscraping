9bb9cb779896bbea175d5490abf8861af2416392
Crisslope01 <122842614+Crisslope01@users.noreply.github.com>
2023-09-06 00:27:49 -0300

    sct
    act

---
+++ scraping_n2/tripadvisor.py
@@ -0,0 +1,9 @@
+from scrapy.item import Field
+from scrapy.item import Item
+from scrapy.spiders import Spider
+from scrapy.selector import Selector
+from scrapy.loader import ItemLoader
+
+
+#scraping Vertical
+
