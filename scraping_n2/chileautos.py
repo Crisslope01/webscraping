b0df4cf56274cb3f99fbc046a30b2b6339fd6ef1
Crisslope01 <122842614+Crisslope01@users.noreply.github.com>
2023-09-12 22:18:03 -0300

    act
    act

--- scraping_n2/chileautos.py
+++ scraping_n2/chileautos.py
@@ -12,12 +12,12 @@ from scrapy.loader import ItemLoader
 
 class FordF150(Item):
     precio = Field()
-    modeloMarca: Field()
-    especificaciones: Field()
-    comentarios: Field()
+    modeloMarca = Field()
+    especificaciones = Field()
+    comentarios = Field()
     
 class ChileAutos(CrawlSpider):
-    name: "web_scraping"
+    name = "SpiderChileAutos"
     
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36'
@@ -31,7 +31,7 @@ class ChileAutos(CrawlSpider):
             LinkExtractor(
                 allow=r'/detalles/'
             ), follow=True, callback="parse_fordf150"
-        )
+        ),
     )
     
     def parse_fordf150(self, response):
@@ -39,10 +39,17 @@ class ChileAutos(CrawlSpider):
         item = ItemLoader(FordF150(), sel)
         
         item.add_xpath('precio', '//section[@class="price-display-wrapper"]//div[@class="price"]/text()')
-        item.add_xpath('modeloMarca', '//div[@class="col-lg-8 col-sm-10"]/h1/text()')
+        item.add_xpath('modeloMarca','//section/div[@class="details-wrapper"]//div//div[@class="col-lg-8 col-sm-10"]/h1/text()')
         item.add_xpath('especificaciones', '//div[@class="key-details-wrapper"]//div[@class="key-details-item-title"]/text()')
         item.add_xpath('comentarios', '//section[@class= "comments"]//div[@class="view-more-target"]/p/text()')
         yield item.load_item()
+        
+        
+# EJECUCION EN TERMINAL:
+# scrapy runspider chileautos.py -o chileautosfinal.csv -t csv   
+# scrapy runspider chileautos.py -o chileautostitulares.csv -t csv  
+# scrapy runspider chileautos.py -o chileautosdescripcion.csv -t csv 
+# scrapy runspider chileautos.py -o chileautosfinal.json -t json
         
         
        
