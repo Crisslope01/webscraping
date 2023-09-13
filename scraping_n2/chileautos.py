c77375377068bed78d16b28ff16a8d3fe01704f6
Crisslope01 <122842614+Crisslope01@users.noreply.github.com>
2023-09-13 11:32:01 -0300

    act
    act

--- scraping_n2/chileautos.py
+++ scraping_n2/chileautos.py
@@ -33,12 +33,14 @@ class ChileAutos(CrawlSpider):
             ), follow=True, callback="parse_fordf150"
         ),
     )
+    def quitarSimboloPeso(self, texto):
+        nuevoTexto = texto.replace("$", "")
+        return nuevoTexto
     
     def parse_fordf150(self, response):
         sel = Selector(response)
         item = ItemLoader(FordF150(), sel)
-        
-        item.add_xpath('precio', '//section[@class="price-display-wrapper"]//div[@class="price"]/text()')
+        item.add_xpath('precio', '//section[@class="price-display-wrapper"]//div[@class="price"]/text()', MapCompose(self.quitarSimboloPeso))
         item.add_xpath('modeloMarca','//section/div[@class="details-wrapper"]//div//div[@class="col-lg-8 col-sm-10"]/h1/text()')
         item.add_xpath('especificaciones', '//div[@class="key-details-wrapper"]//div[@class="key-details-item-title"]/text()')
         item.add_xpath('comentarios', '//section[@class= "comments"]//div[@class="view-more-target"]/p/text()')
